9512857c864cabbd3675238d773c48c0f021d28a
techiemanish <manishtiwari627@gmail.com>
2022-03-12 23:31:16 +0530

    Cross Origin added for React.js Configuration

--- src/main/java/com/springbootmongodb/springbootmongodb/Controllers/MyController.java
+++ src/main/java/com/springbootmongodb/springbootmongodb/Controllers/MyController.java
@@ -5,6 +5,7 @@ import com.springbootmongodb.springbootmongodb.Entity.Student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = { "http://localhost:3000" })
 public class MyController {
     @Autowired
     private StudentDao studentDao;
